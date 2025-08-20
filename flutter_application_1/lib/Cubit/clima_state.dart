cd494cde8a7564974300a0dd966f93cf5ec81099
Pacheco05Jesus <pachecobarrios7@gmail.com>
2025-08-20 16:25:24 -0500

    modificaciones

--- flutter_application_1/lib/Cubit/clima_state.dart
+++ flutter_application_1/lib/Cubit/clima_state.dart
@@ -1,6 +1,11 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import 'package:flutter_application_1/Modelo/Clima.dart';
 =======
+=======
+
+abstract class ClimaEstado {}
+>>>>>>> Stashed changes
 
 abstract class ClimaEstado {}
 >>>>>>> Stashed changes
